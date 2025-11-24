d7e4a04ddfdaa9445c1426ab83db389cd317fbd7
jostin-tech <jostink47@gmail.com>
2025-11-24 12:41:22 +0100

    Composant playing card ajouter

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCard } from "./components/playing-card/playing-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [PlayingCard]
 })
 export class App {
   protected readonly title = signal('playing-cards');
